1735cbc7f168705251a241068333845915c91d80
Aydan <aydan2903@gmail.com>
2022-08-10 16:41:45 +0400

    movielist cards ui ready

--- src/companents/MovieList/MovieList.jsx
+++ src/companents/MovieList/MovieList.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import './_movieList.scss'
+import SingleMovie from'../singleMovie/SingleMovie'
 
 export default function MovieList() {
+
   return (
-    <div>MovieList</div>
+    <div className="movies">
+      <h1>Movies</h1>
+      <div className="list">
+      {[...new Array(6)].map((movie,index)=>{
+        return  <SingleMovie key={index}/>
+     
+      })}
+      </div>
+     
+    </div>
   )
 }
---
+++ src/companents/singleMovie/SingleMovie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './_singleMovie.scss'
+
+export default function SingleMovie() {
+  return (
+    <div className="movie">
+      <div className="movie-img"><img src="https://dmitryvolkov.me/demo/flixgo2.0/main/img/covers/cover.jpg" alt="" /></div>
+      <h3>name</h3>
+    </div>
+  )
+}
