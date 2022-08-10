618797315b077161e9026ea0e7296d3247a40b9e
Aydan <aydan2903@gmail.com>
2022-08-10 17:00:48 +0400

    footer ui is ready

---
+++ src/companents/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./_footer.scss"
+
+export default function Footer() {
+  return (
+    <div className="footer">
+<div className="footer__container">
+  <h3>© Lorem, ipsum dolor. 2022. Lorem, ipsum dolor..</h3>
+  <ul>
+    <li>Terms of Use</li>
+    <li>Privacy Policy</li>
+  </ul>
+</div>
+    </div>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../../companents/Navbar/Navbar'
 import MovieList from '../../companents/MovieList/MovieList'
 import Search from '../../companents/Search/Search'
-import Footer from '../../companents/Footer'
+import Footer from '../../companents/footer/Footer'
 import './_home.scss'
 
 export default function Home() {
